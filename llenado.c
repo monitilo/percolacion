5dd5eca5a217b0df9db548f70e6be9dbcca76287
German Agustin Chiarelli <gchiarelli@ws22.labo5>
2017-03-31 16:03:56 -0300

    editado el llenado

--- llenado.c
+++ llenado.c
@@ -3,31 +3,33 @@
 #include <stdlib.h>
 #include <time.h>
 
-void llenar (int* red,int n, int m, float p);
+void llenar (int* red,int n, int m, float p); //para poblar la red de n*m con proba p en cada punto
+void print_red (int* red,int n, int m);				//para printear la red en forma de matriz en la consola
 
 int main()
 {
+
    time_t t;
    /* Intializes random number generator */
    srand((unsigned) time(&t));
-	int* red;
-	int n;
-	int m;
-	float p;
-	p= 0.1	;
-	n=70 ;
-	m=15 ;
+
+	int* red; //Marcador de memoria para crear la matiz de la red
+	int n;     //cantidad de filas
+	int m;			// cantidad de columnas
+	float p;			//porbabilidad de poblar un lugar (entre 0 y 1)
+	p= 0.5	;
+	n=10 ;
+	m=10 ;
 	red=malloc(n*m* sizeof(int));
 
 	llenar(red,n,m,p);
-
+	print_red(red,n,m);
    
    return(0);
 }
 
 void llenar (int*red,int n, int m, float p){
 	int i;
-	int j;
 	int r;
 
 	for (i=0;i<n*m;i++){
@@ -42,13 +44,19 @@ void llenar (int*red,int n, int m, float p){
 			red[i]=1;
 		}
 	}
-	for(i=0 ;i<m;i=i+1){
 
-		for(j=0;j<n;j=j+1){ 
+}
 
-			printf("%d", red[i*n+j]);
+void print_red(int* red, int n, int m)
+{
+	int i;
+	int j;
+	for(i=0 ;i<n;i=i+1){
+
+		for(j=0;j<m;j=j+1){ 
+
+			printf("%d", red[i*m+j]);
 		}
 		printf("\n");
 	}
 }
-
