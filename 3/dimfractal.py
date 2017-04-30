4aff9a47c8d4eaae9350b37178a4c2a9f651a06a
Facundo Luciano Sanchez <flsanchez94@gmail.com>
2017-04-29 22:59:46 -0300

    Cambios: En 1.d, adherido el programa para calcular nu,pc(inf) y beta En 3, mejorada la eficiencia de dimfractal.py. Ademas, se añadio sigma a los datos En 6, mejorado el programa para hallar gamma (todavia no matcheo)

--- 3/dimfractal.py
+++ 3/dimfractal.py
@@ -9,6 +9,7 @@ ls=os.listdir(cwd)
 l=np.array([])
 p=np.array([])
 m=np.array([])
+sigma=m
 
 
 start=99638
@@ -19,60 +20,41 @@ index=np.arange(12,127,2)
 for files in ls:
 
 	if files.endswith(".txt"):
-
-		for h in index:
 			
-			f = open(files,"r")
+		f = open(files,"r")
 
-			for j in range(0,start):
-				f.readline()
+		for j in range(0,start):
+			f.readline()
+		
+		for h in index:
 
 			l0=0
 			p0=0
+			p0sqr=0
 			m0=0
-			z=0
-			
-			for line in f:
-		
-				aux=line.split(";")
-				l0=int(aux[0])
-				if(l0==h):
-					p0=float(aux[1])+p0
-					m0=float(aux[2])+m0
-					z=z+1
-					
+			z=100000
 
-			l=np.append(l,h)
+			for j in range(0,z):
+
+				aux=f.readline()
+				aux=aux.split(";")
+				l0=int(aux[0])
+				p0=float(aux[1])+p0
+				m0=float(aux[2])+m0
+				p0sqr=float(aux[1])**2+p0sqr
+			
+
+			l=np.append(l,l0)
 			p=np.append(p,p0/z)
 			m=np.append(m,m0/z)
-			print "L = " + str(h) + " , Z = " + str(z)
-		
-			"""i=0
-			for line in f:
-			
-				if(i!=z):
-				
-					i=i+1
-					aux=line.split(";")
-					l0=int(aux[0])
-					p0=float(aux[1])+p0
-					m0=float(aux[2])+m0
+			sigma=np.append(sigma, p0sqr/z - (p0/z)**2)
+			print "L = " + str(l0) + " , Z = " + str(z)
+	
 
-				else:
-					print l0
-					i=0
-					l=np.append(l,l0)
-					p=np.append(p,p0/z)
-					m=np.append(m,m0/z)
-					aux=line.split(";")
-					l0=int(aux[0])
-					p0=float(aux[1])
-					m0=float(aux[2])"""
-
-			f.close()
+		f.close()
 				
 
-np.savetxt('lpm.out1', [l,p,m], fmt='%1.6f', header='1:L;2:<P>;3:<M>')				
+np.savetxt('lpmsigma.txt', [l,p,m,sigma], fmt='%1.6f', header='1:L;2:<P>;3:<M>;4:sigma')				
 			
 			
 			
--- 6/graphgamma.py
+++ 6/graphgamma.py
@@ -1,61 +1,69 @@
 import matplotlib.pyplot as plt
+import matplotlib.lines as lines
 import sys
 import numpy as np
+import scipy.stats as sp
 import os
 
 cwd=os.getcwd()
 ls=os.listdir(cwd)
 
-def ajuste(x,y,inf,up):
-
-	xfit=np.array([])
-	yfit=xfit
-	i=0
-
-	while(x[i] <= inf):
-		i=i+1
-
-	while(x[i] <= up):
-		xfit=np.append(xfit,x[i])
-		yfit=np.append(yfit,y[i])
-		i=i+1
-
-	p=sp.linregress(xfit,yfit)
-	plt.plot(xfit,yfit,'go',xfit,xfit*p[0]+p[1])
-	plt.xlabel("p")
-	plt.ylabel(r"\P_inf")
-	plt.title("Fuerza de cluster percolante P_inf")
-	plt.show()
-	return p
-
 p=np.array([])
 m2=np.array([])
+cmenos=m2
+cmas=m2
+r2mas=m2
+r2menos=m2
+deltap=m2
 
 for files in ls:
 
 	if files.endswith(".out"):
 		
 		f=open(files,"r")
-		p,m2=np.loadtxt(files,dtype='float')
+		p,m2=np.loadtxt(files)
 
-		imax=np.where(m2==np.max(m2))
-		imax=int(imax[0])
+		imax=np.where(m2==np.max(m2))	
+		imax=int(imax[0])+150
 		pmax=p[imax]
-		
+		print pmax
 
-		imenor=imax-600
-		imayor=imax+600
-	
-		cmas=np.log(np.diff(m2[(imax+1):(imayor+1)])/np.diff(p[(imax+1):(imayor+1)]))
-		cmenos=np.log(np.diff(m2[imenor:imax])/np.diff(p[imenor:imax]))
-		cmenos=np.flipud(cmenos)
-		
-		x=np.log(np.abs(np.array(p[imax:(imayor-1)])-pmax))
+		d=10  
+		off=0
+
+		for i in range(1,740/d+1):
+			
+			x = np.log(np.abs(p[imax+1+off:imax+i*d+1+off]-pmax))
+			y = np.log(m2[imax+1+off:imax+i*d+1+off])
+			aux=sp.linregress(x,y)
+			cmas=np.append(cmas,aux[0])
+			r2mas=np.append(r2mas,aux[2]**2)
+			
+			x = np.log(np.abs(p[imax-i*d-off:imax-off]-pmax))
+			y = np.log(m2[imax-i*d-off:imax-off])
+			aux=sp.linregress(x,y)
+			cmenos=np.append(cmenos,aux[0])
+			r2menos=np.append(r2menos,aux[2]**2)	
+
+			deltap=np.append(deltap,np.abs(p[imax+i*d]-pmax))
 				
 		plt.figure()
 		plt.plot(p,m2,'ro',p[imax],m2[imax],'go')
+		lines.Line2D(np.array([p[imax],p[imax]]),np.array([0,m2[imax]]))
+		plt.xlabel("p")
+		plt.ylabel("$M_2$")
+		plt.title("$M_2$ vs. p")
 		plt.figure()
-		plt.plot(x,cmas,'ro',x,cmenos,'go')
+		plt.subplot(211)
+		plt.plot(deltap,cmas,'ro-', label="$\gamma_+$")
+		plt.plot(deltap,cmenos,'go-', label="$\gamma_-$")
+		plt.legend()
+		plt.title("$\gamma$")
+		plt.subplot(212)
+		plt.plot(deltap,r2mas,'ro-', label="$R^2+$")
+		plt.plot(deltap,r2menos,'go-', label="$R^2-$")
+		plt.legend()
+		plt.title("$R^2$")
 		plt.show()
 
 
